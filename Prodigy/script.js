587fcf937a6247dffdfc129569c91895ac9961a8
Maitri Trivedi <106161743+Mae3ri@users.noreply.github.com>
2024-07-20 13:31:41 +0530

    Add files via upload

---
+++ Prodigy/script.js
@@ -0,0 +1,7 @@
+function scrollHeader() {
+    const header = document.getElementById('header')
+
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+
+}
+window.addEventListener('scroll', scrollHeader)
